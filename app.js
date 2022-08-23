42582a756bafdec6cd7740d0a5632f6c24ad98f9
Albert Tortello <altomoaj2018@gmail.com>
2022-08-23 14:26:44 -0500

    newchanges

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(5000 || process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("escuchando");
 });
--- config/database.js
+++ config/database.js
@@ -1,8 +1,14 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "conffiguracion",
   database: "contracts_database",
+  user: "8s00ut7smv9z",
+  host: "wb27jpj4hb4i.us-east-1.psdb.cloud",
+  password: "pscale_pw_CzeJDJXzxFz10xqr5thj9DCn3N_QFxAVkM-G-uNclIs",
 });
+// export const pool = createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "conffiguracion",
+//   database: "contracts_database",
+// });
