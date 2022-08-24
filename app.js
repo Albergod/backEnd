38b7c2db8a9085dae4744a40ac5c86762a635b34
Albert Tortello <altomoaj2018@gmail.com>
2022-08-23 21:18:31 -0500

    new

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.get("/api", routes);
+app.get("/api", routes.get());
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("escuchando");
