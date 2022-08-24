90ad6a948013615158559292c8a2dbc941216832
Albert Tortello <altomoaj2018@gmail.com>
2022-08-23 20:31:24 -0500

    New commit

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
+app.get("/", (req, res) => {
+  res.send("Succefuly");
+});
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("escuchando");
 });
